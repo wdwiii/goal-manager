bd745caf4888cb57481c86cdd784540a18b206fb
Willie Whitfield <71739892+wdwiii@users.noreply.github.com>
2021-10-30 01:22:00 -0400

    Use state for course input form
    - Set inital isValid state to true
    - Call setIsValid inside goalInputChangeHandler and formSubmitHandler
    - Set dynamic styles for form control label and input

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -5,21 +5,38 @@ import './CourseInput.css';
 
 const CourseInput = props => {
   const [enteredValue, setEnteredValue] = useState('');
-
+  const [isValid, setIsValid] = useState(true);
   const goalInputChangeHandler = event => {
+    if (event.target.value.trim().length > 0) {
+      setIsValid(true);
+      //If input value length is greater than one, set isValid state to true
+    }
     setEnteredValue(event.target.value);
   };
 
   const formSubmitHandler = event => {
     event.preventDefault();
+    if (enteredValue.trim().length === 0) {
+      setIsValid(false);
+      return;
+      //If form is submitted with 0 characters, set isValis state to false and return nothing
+    }
     props.onAddGoal(enteredValue);
   };
 
   return (
     <form onSubmit={formSubmitHandler}>
       <div className="form-control">
-        <label>Course Goal</label>
-        <input type="text" onChange={goalInputChangeHandler} />
+        <label style={{ color: !isValid ? 'red' : 'black' }}>Course Goal</label>
+        <input
+          type="text"
+          onChange={goalInputChangeHandler}
+          style={{
+            backgroundColor: !isValid ? 'pink' : 'white',
+            borderColor: !isValid ? 'red' : 'black',
+          }}
+          //^^ If isValid state is NOT true, styles for label and input will be active
+        />
       </div>
       <Button type="submit">Add Goal</Button>
     </form>
