dd30d0c98ed2d18f2868bcbbfcf12f4e943db6fb
Willie Whitfield <71739892+wdwiii@users.noreply.github.com>
2021-10-30 11:31:00 -0400

    Convert div.form-control into styled component
    - Delete CourseInput.css file
    - Create FormControl styled component
    - Attatch form-control styles to component
    - Adapt invalid styles based on props

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,7 +1,36 @@
 import React, { useState } from 'react';
+import styled from 'styled-components';
 
 import Button from '../../UI/Button/Button';
-import './CourseInput.css';
+
+const FormControl = styled.div`
+  margin: 0.5rem 0;
+
+  & label {
+    font-weight: bold;
+    display: block;
+    margin-bottom: 0.5rem;
+
+    /* Adapt the colors based on invalid prop */
+    color: ${props => (props.invalid ? 'red' : 'black')};
+  }
+
+  & input {
+    background-color: ${props => (props.invalid ? 'pink' : 'transparent')};
+    display: block;
+    width: 100%;
+    border: 1px solid ${props => (props.invalid ? 'red' : '#ccc')};
+    font: inherit;
+    line-height: 1.5rem;
+    padding: 0 0.25rem;
+  }
+
+  & input:focus {
+    outline: none;
+    background: #fad0ec;
+    border-color: #8b005d;
+  }
+`;
 
 const CourseInput = props => {
   const [enteredValue, setEnteredValue] = useState('');
@@ -29,14 +58,14 @@ const CourseInput = props => {
       {/* Dynamic way of adding classes
       Write template literal inside curly braces adding invakid className on the condition that the current isVlid state is false
        */}
-      <div className={`form-control ${!isValid ? 'invalid' : ''}`}>
+      <FormControl invalid={!isValid}>
         <label>Course Goal</label>
         <input
           type="text"
           onChange={goalInputChangeHandler}
           //^^ If isValid state is NOT true, styles for label and input will be active
         />
-      </div>
+      </FormControl>
       <Button type="submit">Add Goal</Button>
     </form>
   );
